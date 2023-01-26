1129762ed8012f9daafc8aabe64808de47a15e90
Adenugakolade-02 <kolade2.adenuga@gmail.com>
2023-01-26 23:37:17 +0100

    made changes to signup UI

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,41 +1,44 @@
 import 'dart:async';
 
-
 // import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 // import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutterfire_ui/auth.dart';
 import 'package:flutter/material.dart';
-import 'firebase_options.dart';
+// import 'firebase_options.dart';
 
 class AuthScreens extends StatelessWidget {
   const AuthScreens({super.key});
 
   @override
   Widget build(BuildContext context) => StreamBuilder<User?>(
-    stream: FirebaseAuth.instance.authStateChanges(),
-    builder: (context, snapshot) {
-      if(snapshot.hasData){
-        debugPrint("email ${snapshot.data?.email}");
-        debugPrint("userid ${snapshot.data?.uid}");
-        return const HomeScreen();
-      }
-      else{
-        return const SignInScreen(
-          providerConfigs: [
-            EmailProviderConfiguration(),
-            GoogleProviderConfiguration(
-              clientId: "586882765354-orh5e3iv5agqf29de0hqj4vf90dssqbq.apps.googleusercontent.com")
-              
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          debugPrint("email ${snapshot.data?.email}");
+          debugPrint("userid ${snapshot.data?.uid}");
+          return const HomeScreen();
+        } else {
+          return SignInScreen(
+            headerBuilder: (context, constraints, shrinkOffset) {
+              return Padding(
+                padding: const EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image.asset('assets/images/google-keep.png'),
+                ),
+              );
+            },
+            providerConfigs: const [
+              EmailProviderConfiguration(),
+              GoogleProviderConfiguration(
+                  clientId:
+                      "586882765354-orh5e3iv5agqf29de0hqj4vf90dssqbq.apps.googleusercontent.com")
             ],
-        );
-      }
-    }
-    );
-  // {
-  //   return Container();
-  // }
+          );
+        }
+      });
 }
 
 class HomeScreen extends StatelessWidget {
@@ -44,13 +47,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(actions: <Widget>[
-        SignOutButton()
-      ]),
+      appBar: AppBar(actions: <Widget>[SignOutButton()]),
       body: Column(
-        children: const [
-          Text("Welcome to the home screen")
-        ],
+        children: const [Text("Welcome to the home screen")],
       ),
     );
   }
