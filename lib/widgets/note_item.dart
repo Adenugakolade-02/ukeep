abdfcf4de1784fcf3f22c26dc9b068a494d1591d
Adenugakolade-02 <kolade2.adenuga@gmail.com>
2023-02-13 08:54:26 +0100

    built note item widget

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,11 +1,49 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:ukeep/models.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
+  final Note note;
+
+  const NoteItem({Key? key, required this.note}):super(key:key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  Widget build(BuildContext context) => Hero(
+    tag: 'Note${note.id}', 
+    child: DefaultTextStyle(
+      style: const TextStyle(
+        color: Color(0xC2000000),
+        fontSize: 14,
+        height: 1.3125
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: note.color,
+          borderRadius: const BorderRadius.all(Radius.circular(6)),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            if (note.title.isNotEmpty ==true)
+            Text(
+              note.title,
+              style: const TextStyle(
+                color: Color(0xFF202124),
+                fontSize: 16,
+                height: 19 / 16,
+                fontWeight: FontWeight.w500,
+                ),
+            ),
+            if (note.title.isNotEmpty ==true) const SizedBox(height: 14,),
+            Flexible(
+              flex: 1,
+              child: Text(note.text))
+          ],
+        ),
+      )
+      )
+    );
 }
